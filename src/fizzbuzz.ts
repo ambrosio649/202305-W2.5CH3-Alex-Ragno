f93d2b7bdcacf14a7b87ec1dd6d92f8b43cdebfa
AlexRagno <alexragno4@gmail.com>
2023-05-10 20:26:31 +0200

    Add fizzbuzz.ts and testsç

---
+++ src/fizzbuzz.ts
@@ -0,0 +1,16 @@
+export const fizzBuzz = (startNumber: number, finalNumber: number) => {
+  const list: any[] = [];
+  for (let index = startNumber; index < finalNumber + 1; index++) {
+    if (index % 5 === 0 && index % 3 === 0) {
+      list.push("FizzBuzz");
+    } else if (index % 3 === 0) {
+      list.push("Fizz");
+    } else if (index % 5 === 0) {
+      list.push("Buzz");
+    } else if (index % 5 !== 0 && index % 3 !== 0) {
+      list.push(index);
+    }
+  }
+
+  return list;
+};
